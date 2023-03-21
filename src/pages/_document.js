ea362e2a7b40a15c6eb2d1c708c3de04f105c819
abraham <abraham.turnsek@gmail.com>
2023-03-21 14:18:54 +0100

    añadir cookies

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,6 +5,20 @@ export default function Document() {
   return (
     <Html lang="es">
       <link rel="icon" href="../../assets/favicon.png" />
+      <Script
+        strategy="lazyOnload"
+        id="Cookiebot"
+        src="https://consent.cookiebot.com/uc.js"
+        data-cbid="66a968ad-70de-4703-81be-84844ed28395"
+        data-blockingmode="auto"
+        type="text/javascript"
+      ></Script>
+      <Script
+        strategy="lazyOnload"
+        id="CookieDeclaration"
+        src="https://consent.cookiebot.com/66a968ad-70de-4703-81be-84844ed28395/cd.js"
+        type="text/javascript"
+      ></Script>
       <Head />
       <body>
         <Script id="livechat" strategy="afterInteractive">
